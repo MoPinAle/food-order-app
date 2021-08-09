f2a1f133d1cc926b0199ad32d105c8678fffc5ce
Zucas <PaivaTech@hotmail.com>
2021-08-09 23:52:18 +0100

    Ajusted the CartItem and Cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,36 @@
-import { link } from 'fs';
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Modal from '../UI/Modal';
 
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+        
+    }
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem()
+    }
+
     const cartItems = (
         <ul className={classes['cart-items']}>
-            {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map(item => (
-                <li>{item.name}</li>
+            {cartCtx.items.map(item => (
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    amount={item.amount} 
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             ))}
         </ul>
     );
@@ -19,7 +40,7 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button 
@@ -27,7 +48,7 @@ const Cart = (props) => {
                     onClick={props.onClose}>
                     Close
                 </button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );

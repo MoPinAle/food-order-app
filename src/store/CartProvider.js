2a1de4348d5706ff71215c9776d57c2daf9ffbbe
Zucas <PaivaTech@hotmail.com>
2021-08-21 00:15:18 +0100

    Cart and Checkout

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -54,6 +54,10 @@ const cartReducer = (state, action) => {
         }
     }
 
+    if (action.type === 'CLEAR') {
+        return defaultCartState;
+    }
+
     return defaultCartState;
 };
 
@@ -74,11 +78,16 @@ const CartProvider = (props) => {
         });
     };
 
+    const clearCartHandler = () => {
+        dispatchCartAction({type: 'CLEAR'})
+    }
+
     const cartContext = {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
+        removeItem: removeItemFromCartHandler,
+        clearCart: clearCartHandler
     };
 
     return (
